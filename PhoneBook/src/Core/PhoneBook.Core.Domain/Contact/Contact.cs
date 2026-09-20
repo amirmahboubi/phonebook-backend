52e1dee6301a6335fc320dbb2b4ce0f8348b212f
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 07:56:32 +0330

    ✨ feat(domain): add Contact

---
+++ PhoneBook/src/Core/PhoneBook.Core.Domain/Contact/Contact.cs
@@ -0,0 +1,29 @@
+﻿namespace PhoneBook.Core.Domain.Contact;
+
+public sealed class Contact
+{
+    private Contact(string firstName,
+                    string lastName,
+                    string phoneNumber,
+                    string tag)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Tag = tag;
+    }
+
+    public Guid Id { get; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string Tag { get; private set; }
+
+    public static Contact Create(string firstName,
+                                 string lastName,
+                                 string phoneNumber,
+                                 string tag)
+    {
+        throw new NotImplementedException();
+    }
+}
