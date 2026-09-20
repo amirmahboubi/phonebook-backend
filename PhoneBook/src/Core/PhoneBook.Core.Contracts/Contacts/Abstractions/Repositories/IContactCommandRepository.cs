d779ed743ff674243664a6c1df920b8e824bbe94
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 17:23:01 +0330

    ✨ feat(contracts): define contact command repository port

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Abstractions/Repositories/IContactCommandRepository.cs
@@ -0,0 +1,10 @@
+﻿using PhoneBook.Core.Domain.Contacts;
+
+namespace PhoneBook.Core.Contracts.Contacts.Abstractions.Repositories;
+
+public interface IContactCommandRepository
+{
+    void Add(Contact contact);
+    void Update(Contact contact);
+    void Delete(Contact contact);
+}
