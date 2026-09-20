0c352b46216c67944f704e36dbb540649d60ce1b
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 17:02:04 +0330

    ✨ feat(contracts): add update contact command

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Commands/UpdateContactCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneBook.Core.Contracts.Contacts.Commands;
+
+public sealed record UpdateContactCommand(
+    Guid ContactId,
+    string? FirstName,
+    string? LastName,
+    string? PhoneNumber,
+    string? Tag);
