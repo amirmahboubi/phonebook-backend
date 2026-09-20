2b7b65e66f03d83ce69dfda92aa20638b00b08bf
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 09:01:15 +0330

    ✨ feat(domain): add phone number value object

---
+++ PhoneBook/src/Core/PhoneBook.Core.Domain/Common/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace PhoneBook.Core.Domain.Common.ValueObjects;
+
+public sealed record PhoneNumber
+{
+    public string Value { get; }
+
+    private PhoneNumber(string value)
+    {
+        Value = value;
+    }
+
+    public static PhoneNumber Create(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new DomainException("Phone number cannot be empty.");
+        }
+
+        return new PhoneNumber(value.Trim());
+    }
+
+    public override string ToString() => Value;
+}
