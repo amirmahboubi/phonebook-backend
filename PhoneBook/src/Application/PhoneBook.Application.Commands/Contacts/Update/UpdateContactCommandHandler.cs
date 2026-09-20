69a15d9f15cd85644dd018238613c97f059dc9ab
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 21:13:46 +0330

    ✨ feat(application): implement update contact command handler

---
+++ PhoneBook/src/Application/PhoneBook.Application.Commands/Contacts/Update/UpdateContactCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using PhoneBook.Core.Contracts.Contacts.Commands;
+using PhoneBook.Application.Commands.Common.Exceptions;
+using PhoneBook.Core.Contracts.Contacts.Abstractions.Repositories;
+
+namespace PhoneBook.Application.Commands.Contacts.Update;
+
+public sealed class UpdateContactCommandHandler
+{
+    private readonly IContactCommandRepository _commandRepository;
+    private readonly IContactQueryRepository _queryRepository;
+    private readonly UpdateContactCommandValidator _validator;
+
+    public UpdateContactCommandHandler(
+        IContactCommandRepository commandRepository,
+        IContactQueryRepository queryRepository,
+        UpdateContactCommandValidator validator)
+    {
+        _commandRepository = commandRepository;
+        _queryRepository = queryRepository;
+        _validator = validator;
+    }
+
+    public void Handle(
+        UpdateContactCommand command)
+    {
+        var validationResult =
+            _validator.Validate(command);
+
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(
+                validationResult.Errors);
+        }
+
+        var contact =
+            _queryRepository.GetById(command.ContactId);
+
+        if (contact is null)
+        {
+            throw new ContactNotFoundException(
+                command.ContactId);
+        }
+
+        contact.Update(
+            command.FirstName,
+            command.LastName,
+            command.PhoneNumber,
+            command.Tag);
+
+        _commandRepository.Update(contact);
+    }
+}
---
+++ PhoneBook/tests/PhoneBook.UnitTests/Application/Commands/Contacts/Update/UpdateContactCommandHandlerTests.cs
@@ -0,0 +1,158 @@
+﻿using PhoneBook.Core.Domain.Contacts;
+using PhoneBook.Core.Contracts.Contacts.Commands;
+using PhoneBook.Application.Commands.Contacts.Update;
+using PhoneBook.Application.Commands.Common.Exceptions;
+using PhoneBook.UnitTests.Application.TestDoubles.Contacts;
+
+namespace PhoneBook.UnitTests.Application.Commands.Contacts.Update;
+
+public sealed class UpdateContactCommandHandlerTests
+{
+    [Fact]
+    public void Handle_WithExistingContact_ShouldUpdateContact()
+    {
+        // Arrange
+        var repository = new FakeContactRepository();
+        var validator = new UpdateContactCommandValidator();
+
+        var contact = Contact.Create(
+            "Pouya",
+            "Mahboubi",
+            "09121234567",
+            "Work");
+
+        repository.AddExisting(contact);
+
+        var handler = new UpdateContactCommandHandler(
+            repository,
+            repository,
+            validator);
+
+        var command = new UpdateContactCommand(
+            contact.Id,
+            "Ali",
+            "Ahmadi",
+            "09129876543",
+            "Friend");
+
+        // Act
+        handler.Handle(command);
+
+        // Assert
+        var updated = repository.GetById(contact.Id);
+
+        Assert.NotNull(updated);
+        Assert.Equal(contact.Id, updated.Id);
+        Assert.Equal("Ali", updated.FirstName);
+        Assert.Equal("Ahmadi", updated.LastName);
+        Assert.Equal("09129876543", updated.PhoneNumber.Value);
+        Assert.Equal("Friend", updated.Tag.Value);
+
+        Assert.Equal(1, repository.UpdateCallCount);
+    }
+
+    [Fact]
+    public void Handle_WithNonExistingContact_ShouldThrowContactNotFoundException()
+    {
+        // Arrange
+        var repository = new FakeContactRepository();
+        var validator = new UpdateContactCommandValidator();
+
+        var handler = new UpdateContactCommandHandler(
+            repository,
+            repository,
+            validator);
+
+        var contactId = Guid.NewGuid();
+
+        var command = new UpdateContactCommand(
+            contactId,
+            "Ali",
+            "Ahmadi",
+            "09129876543",
+            "Friend");
+
+        // Act
+        var action = () => handler.Handle(command);
+
+        // Assert
+        var exception =
+            Assert.Throws<ContactNotFoundException>(action);
+
+        Assert.Equal(
+            contactId,
+            exception.ContactId);
+
+        Assert.Equal(
+            0,
+            repository.UpdateCallCount);
+    }
+
+    [Fact]
+    public void Handle_WithInvalidCommand_ShouldThrowValidationException()
+    {
+        // Arrange
+        var repository = new FakeContactRepository();
+        var validator = new UpdateContactCommandValidator();
+
+        var handler = new UpdateContactCommandHandler(
+            repository,
+            repository,
+            validator);
+
+        var command = new UpdateContactCommand(
+            Guid.Empty,
+            null,
+            "",
+            " ",
+            null);
+
+        // Act
+        var action = () => handler.Handle(command);
+
+        // Assert
+        var exception =
+            Assert.Throws<ValidationException>(action);
+
+        Assert.Equal(5, exception.Errors.Count);
+        Assert.Equal(0, repository.UpdateCallCount);
+    }
+
+    [Fact]
+    public void Handle_ShouldPreserveContactIdentity()
+    {
+        // Arrange
+        var repository = new FakeContactRepository();
+        var validator = new UpdateContactCommandValidator();
+
+        var contact = Contact.Create(
+            "Pouya",
+            "Mahboubi",
+            "09121234567",
+            "Work");
+
+        var originalId = contact.Id;
+
+        repository.AddExisting(contact);
+
+        var handler = new UpdateContactCommandHandler(
+            repository,
+            repository,
+            validator);
+
+        var command = new UpdateContactCommand(
+            originalId,
+            "Sara",
+            "Mohammadi",
+            "09351234567",
+            "Family");
+
+        // Act
+        handler.Handle(command);
+
+        // Assert
+        Assert.Equal(
+            originalId,
+            contact.Id);
+    }
+}
