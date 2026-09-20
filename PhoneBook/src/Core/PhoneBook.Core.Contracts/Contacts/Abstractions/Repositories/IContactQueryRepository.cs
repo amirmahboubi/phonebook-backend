39e2ca72807e7e44ade89441fb00dff16f3cceef
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 17:30:58 +0330

    ✨ feat(contracts): define contact query repository port

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Abstractions/Repositories/IContactQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using PhoneBook.Core.Domain.Contacts;
+
+namespace PhoneBook.Core.Contracts.Contacts.Abstractions.Repositories;
+
+public interface IContactQueryRepository
+{
+    IReadOnlyList<Contact> GetList();
+    Contact? GetById(Guid contactId);
+    IReadOnlyList<Contact> GetByTag(string tag);
+}
