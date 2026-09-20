4b6a020ea6843772ba72181371c1dd3fdf549822
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 17:00:51 +0330

    ✨ feat(contracts): add create contact command

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Commands/CreateContactCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace PhoneBook.Core.Contracts.Contacts.Commands;
+
+public sealed record CreateContactCommand(
+    string? FirstName,
+    string? LastName,
+    string? PhoneNumber,
+    string? Tag);
