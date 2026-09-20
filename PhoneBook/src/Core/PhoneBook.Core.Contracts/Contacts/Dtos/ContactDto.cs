0250e2a6b4d81c9aa8f6622d6189a0515b87c06d
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 17:19:37 +0330

    ✨ feat(contracts): add contact data transfer object

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Dtos/ContactDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneBook.Core.Contracts.Contacts.Dtos;
+
+public sealed record ContactDto(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    string Tag);
