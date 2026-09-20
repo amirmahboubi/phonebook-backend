896d693bee451457a8feaa89ed39509f0e286a79
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-20 07:57:55 +0330

    🧪 test(domain): add ContactTests

---
+++ PhoneBook/tests/PhoneBook.UnitTests/Core/Domain/Contacts/ContactTests.cs
@@ -0,0 +1,30 @@
+﻿using PhoneBook.Core.Domain.Contact;
+
+namespace PhoneBook.UnitTests.Core.Domain.Contacts;
+
+public class ContactTests
+{
+    [Fact]
+    public void Create_WithValidData_ShouldCreateContact()
+    {
+        // Arrange
+        const string firstName = "Amir";
+        const string lastName = "Mahboubi";
+        const string phoneNumber = "09121234567";
+        const string tag = "Work";
+
+        // Act
+        var contact = Contact.Create(
+            firstName,
+            lastName,
+            phoneNumber,
+            tag);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, contact.Id);
+        Assert.Equal(firstName, contact.FirstName);
+        Assert.Equal(lastName, contact.LastName);
+        Assert.Equal(phoneNumber, contact.PhoneNumber);
+        Assert.Equal(tag, contact.Tag);
+    }
+}
