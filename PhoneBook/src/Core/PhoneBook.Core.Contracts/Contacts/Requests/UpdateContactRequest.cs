7b7c26795c3e39607fbf7e911190d83f654ab6bf
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-21 10:38:54 +0330

    ✨ feat(api): add contact request contracts

---
+++ PhoneBook/src/Core/PhoneBook.Core.Contracts/Contacts/Requests/UpdateContactRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace PhoneBook.Core.Contracts.Contacts.Requests;
+
+public sealed record UpdateContactRequest(
+    string? FirstName,
+    string? LastName,
+    string? PhoneNumber,
+    string? Tag);
