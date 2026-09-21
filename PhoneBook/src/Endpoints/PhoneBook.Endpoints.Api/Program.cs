9eb482b6d12072a1581056e4d409aaba07dcf71a
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-21 10:16:26 +0330

    🔗 chore(api): configure application and infrastructure dependency injection

--- PhoneBook/src/Endpoints/PhoneBook.Endpoints.Api/Program.cs
+++ PhoneBook/src/Endpoints/PhoneBook.Endpoints.Api/Program.cs
@@ -1,6 +1,70 @@
+using PhoneBook.Application.Queries.Contacts.GetAll;
+using PhoneBook.Application.Queries.Contacts.GetById;
+using PhoneBook.Application.Queries.Contacts.GetByTag;
+using PhoneBook.Application.Commands.Contacts.Create;
+using PhoneBook.Application.Commands.Contacts.Update;
+using PhoneBook.Application.Commands.Contacts.Delete;
+using PhoneBook.Infrastructure.Data.InMemory.Context;
+using PhoneBook.Infrastructure.Data.InMemory.Contacts.Repositories;
+using PhoneBook.Core.Contracts.Contacts.Abstractions.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddProblemDetails();
+//builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddSingleton<InMemoryDataContext>(
+    static _ =>
+    {
+        var context = new InMemoryDataContext();
+        context.Seed();
+
+        return context;
+    });
+
+builder.Services.AddSingleton<
+    IContactCommandRepository,
+    ContactCommandRepository>();
+
+builder.Services.AddSingleton<
+    IContactQueryRepository,
+    ContactQueryRepository>();
+
+builder.Services.AddSingleton<CreateContactCommandValidator>();
+builder.Services.AddSingleton<UpdateContactCommandValidator>();
+builder.Services.AddSingleton<DeleteContactCommandValidator>();
+
+builder.Services.AddSingleton<GetContactsByTagQueryValidator>();
+builder.Services.AddSingleton<GetContactByIdQueryValidator>();
+
+builder.Services.AddScoped<CreateContactCommandHandler>();
+builder.Services.AddScoped<UpdateContactCommandHandler>();
+builder.Services.AddScoped<DeleteContactCommandHandler>();
+
+builder.Services.AddScoped<GetAllContactsQueryHandler>();
+builder.Services.AddScoped<GetContactByIdQueryHandler>();
+builder.Services.AddScoped<GetContactsByTagQueryHandler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "PhoneBook API v1");
+    });
+}
+
+//app.MapContactEndpoints();
 
 app.Run();
+
+public partial class Program;
