865cef54a8f29d8f7399c43f19124013d4681553
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-21 09:44:45 +0330

    ✨ feat(application): add get contact by id query validator

---
+++ PhoneBook/src/Application/PhoneBook.Application.Queries/Contacts/GetById/GetContactByIdQueryValidator.cs
@@ -0,0 +1,25 @@
+﻿using PhoneBook.Core.Contracts.Contacts.Queries;
+using PhoneBook.Core.Contracts.Common.Validation;
+
+namespace PhoneBook.Application.Queries.Contacts.GetById;
+
+public sealed class GetContactByIdQueryValidator
+{
+    public ValidationResult Validate(
+        GetContactByIdQuery query)
+    {
+        ArgumentNullException.ThrowIfNull(query);
+
+        if (query.ContactId == Guid.Empty)
+        {
+            return new ValidationResult(
+            [
+                new ValidationError(
+                    nameof(query.ContactId),
+                    "Contact ID is required.")
+            ]);
+        }
+
+        return ValidationResult.Success;
+    }
+}
