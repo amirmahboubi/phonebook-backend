bbac9f148c411295ceca772ed382b00260b5276f
Amir Pouya Mahboubi <amirmahboubi@outlook.com>
2026-09-21 09:36:30 +0330

    ✨ feat(application): implement get all contacts query handler

---
+++ PhoneBook/src/Application/PhoneBook.Application.Queries/Contacts/GetAll/GetAllContactsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using PhoneBook.Core.Contracts.Contacts.Dtos;
+using PhoneBook.Core.Contracts.Contacts.Queries;
+using PhoneBook.Core.Contracts.Contacts.Abstractions.Repositories;
+
+namespace PhoneBook.Application.Queries.Contacts.GetAll;
+
+public sealed class GetAllContactsQueryHandler
+{
+    private readonly IContactQueryRepository _repository;
+
+    public GetAllContactsQueryHandler(
+        IContactQueryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public IReadOnlyList<ContactDto> Handle(GetAllContactsQuery query)
+    {
+        ArgumentNullException.ThrowIfNull(query);
+
+        return _repository
+            .GetAll()
+            .Select(contact =>
+                new ContactDto(
+                    contact.Id,
+                    contact.FirstName,
+                    contact.LastName,
+                    contact.PhoneNumber.Value,
+                    contact.Tag.Value))
+            .ToArray();
+    }
+}
